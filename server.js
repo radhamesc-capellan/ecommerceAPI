9d3470f4c8deb9a57e450e62855173d30d09595b
Radhames Capellan <radhamesc@gmail.com>
2022-03-28 22:36:28 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const { app } = require('./app');
+
+//Utils
+const { sequelize } = require('./utils/database');
+
+sequelize
+  .authenticate()
+  .then(() => console.log('Database authenticated'))
+  .catch((err) => console.log(err));
+
+// Database synced with models' relations
+sequelize
+  .sync()
+  .then(() => console.log('Database synced'))
+  .catch((err) => console.log(err));
+
+// Spin up server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Express app running on port: ${PORT}`);
+});
